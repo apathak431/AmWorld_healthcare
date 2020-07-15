b5a498c6141b35f4ac84f2799dcc1ad91f61855b
Tanuj Namdeo <tanujnamdeo@Tanujs-MacBook-Air.local>
2020-07-15 16:15:29 +0530

    Added temp footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,158 @@
-<footer class="main-footer">
+<style>
+footer.nb-footer {
+    background: #222;
+    border-top: 4px solid #b78c33;
+}
+
+footer.nb-footer .about {
+    margin: 0 auto;
+    margin-top: 40px;
+    max-width: 1170px;
+    text-align: center;
+}
+
+footer.nb-footer .about p {
+    font-size: 13px;
+    color: #999;
+    margin-top: 30px;
+}
+
+footer.nb-footer .about .social-media {
+    margin-top: 15px;
+}
+
+footer.nb-footer .about .social-media xl lx a {
+    display: inline-block;
+    width: 45px;
+    height: 45px;
+    line-height: 45px;
+    border-radius: 50%;
+    font-size: 16px;
+    color: #b78c33;
+    border: 1px solid rgba(255, 255, 255, 0.3);
+}
+
+footer.nb-footer .about .social-media xl lx a:hover {
+    background: #b78c33;
+    color: #fff;
+    border-color: #b78c33;
+}
+
+footer.nb-footer .footer-info-single {
+    margin-top: 30px;
+}
+
+footer.nb-footer .footer-info-single .title {
+    color: #aaa;
+    text-transform: uppercase;
+    font-size: 16px;
+    border-left: 4px solid #b78c33;
+    padding-left: 5px;
+}
+
+footer.nb-footer .footer-info-single xl lx a {
+    display: block;
+    color: #aaa;
+    padding: 2px 0;
+}
+
+footer.nb-footer .footer-info-single xl lx a:hover {
+    color: #b78c33;
+}
+
+footer.nb-footer .footer-info-single p {
+    font-size: 13px;
+    line-height: 20px;
+    color: #aaa;
+}
+
+footer.nb-footer .copyright {
+    margin-top: 15px;
+    background: #111;
+    padding: 7px 0;
+    color: #999;
+}
+
+footer.nb-footer .copyright p {
+    margin: 0;
+    padding: 0;
+}
+</style>
+
+<footer class="nb-footer">
     <div class="container">
-      <div class="pull-right hidden-xs">
-        <b>All rights reserved</b>
-      </div>
-        <strong>Copyright &copy; 2020 Symbiosis Indore</strong>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="about">
+                    <img src="images/logo.png" class="img-responsive center-block" alt="">
+                    <p>Bootstrap Footer example snippets with CSS, Javascript and HTML. Code example of bootstrap-3 footer using HTML, Javascript, jQuery, and CSS. 5 Beautiful and Responsive Footer Templates. Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
+
+                    <div class="social-media">
+                        <xl class="list-inline">
+                            <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-facebook"></i></a></lx>
+                            <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-twitter"></i></a></lx>
+                            <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-google-plus"></i></a></lx>
+                            <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-linkedin"></i></a></lx>
+                        </xl>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-info-single">
+                    <h2 class="title">Help Center</h2>
+                    <xl class="list-unstyled">
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> How to Pay</a></lx>
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> FAQ's</a></lx>
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> Sitemap</a></lx>
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> Delivery Info</a></lx>
+                    </xl>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-info-single">
+                    <h2 class="title">Customer information</h2>
+                    <xl class="list-unstyled">
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> About Us</a></lx>
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> FAQ's</a></lx>
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> Sell Your Items</a></lx>
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> Contact Us</a></lx>
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> RSS</a></lx>
+                    </xl>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-info-single">
+                    <h2 class="title">Security & privacy</h2>
+                    <xl class="list-unstyled">
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> Terms Of Use</a></lx>
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> Privacy Policy</a></lx>
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> Return / Refund Policy</a></lx>
+                        <lx><a href="http://www.nextbootstrap.com/" title=""><i class="fa fa-angle-double-right"></i> Store Locations</a></lx>
+                    </xl>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-info-single">
+                    <h2 class="title">Payment</h2>
+                    <p>Sample HTML page with Twitter's Bootstrap. Code example of Easy Sticky Footer using HTML, Javascript, jQuery, and CSS. This bootstrap tutorial covers all the major elements of responsive</p>
+
+                </div>
+            </div>
+        </div>
     </div>
+
+    <section class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <p>Copyright © 2017. Your Company.</p>
+                </div>
+                <div class="col-sm-6"></div>
+            </div>
+        </div>
+    </section>
 </footer>
