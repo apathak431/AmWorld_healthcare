b55badb57dd499034519bfe32073abfd75ff1043
Tanuj Namdeo <tanujnamdeo@Tanujs-MacBook-Air.local>
2020-07-19 22:02:22 +0530

    Contact us page changes

--- contactus.php
+++ contactus.php
@@ -5,7 +5,7 @@ $mailheader = "From: ".$_POST["email"]."\r\n";
 $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
 $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
 $MESSAGE_BODY = "First Name: ".$_POST["firstname"].""; 
-$MESSAGE_BODY = "Last Name: ".$_POST["lastname"]."";
+$MESSAGE_BODY .= "Last Name: ".$_POST["lastname"]."";
 $MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
 $MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
 mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
